2ca014a20d6a42a73cb1a0ca8364a41a1941929c
tanjimTC <tanjimtc74@gmail.com>
2020-09-16 23:57:34 +0600

    components added

---
+++ src/components/jumbotron/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Inner } from "./styles/jumbotron";
+
+const index = () => {
+  return (
+    <div>
+      <Inner>
+        <h1>I am thr jumbo</h1>
+      </Inner>
+    </div>
+  );
+};
+
+export default index;
